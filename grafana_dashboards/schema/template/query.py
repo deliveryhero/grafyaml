4d041901637a2a724234462eca72639cacdd7d34
Ankit Goel <ankit.goel@deliveryhero.com>
2020-08-07 15:36:25 +0530

    Enable regex in query templates

--- grafana_dashboards/schema/template/query.py
+++ grafana_dashboards/schema/template/query.py
@@ -32,6 +32,7 @@ class Query(Base):
                 v.All(int, v.Range(min=0, max=2)),
             v.Optional('datasource'): v.All(str),
             v.Optional('hide'): v.All(int, v.Range(min=0, max=2)),
+            v.Optional('regex'): v.All(str),
         }
         query.update(self.base)
         return v.Schema(query)
