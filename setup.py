473bc2ee209c49fca1d06b8f8dda1405b3907cf5
Max Williams <max.williams@deliveryhero.com>
2020-10-01 17:16:38 +0200

    fix formatting

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name = 'grafyaml',
-    version = '1.0',
-    url = 'https://github.com/deliveryhero/grafyaml',
-    license = 'Apache v2.0',
-    description = 'A nice and easy way to template Grafana dashboards in YAML',
-    long_description = open('README.md', encoding='utf-8').read(),
-    packages = ['grafana_dashboards'],
-    entry_points = {
+    name='grafyaml',
+    version='1.0',
+    url='https://github.com/deliveryhero/grafyaml',
+    license='Apache v2.0',
+    description='A nice and easy way to template Grafana dashboards in YAML',
+    long_description=open('README.md', encoding='utf-8').read(),
+    packages=['grafana_dashboards'],
+    entry_points={
         'console_scripts': ['grafana-dashboard=grafana_dashboards.cmd:main'],
     },
-    install_requires = [
+    install_requires=[
         'dogpile.cache',
         'python-slugify',
         'PyYAML>=3.1.0',
@@ -19,7 +19,7 @@ setup(
         'six>=1.6.0',
         'voluptuous<=0.10.5'
     ],
-    classifiers = [
+    classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
